06bd41bff3ab266216ceaa7d55e183d363495217
agnesockernahl <agnes.ockernahl@payex.com>
2023-01-23 16:47:50 +0100

    changes

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -3,6 +3,7 @@ import Restaurant from "./Restaurant";
 
 function Restaurants() {
   const [restaurants, setRestaurants] = useState([]);
+  const [randomRestaurant, setRandomRestaurant] = useState({});
   const fetchRestaurantsData = () => {
     fetch(
       "https://hotell.difi.no/api/json/mattilsynet/smilefjes/tilsyn?poststed=trondheim"
@@ -11,6 +12,14 @@ function Restaurants() {
       .then((result) => setRestaurants(result.entries))
       .catch((error) => console.log("error", error));
   };
+
+  const getRandomRestaurant = (restaurants) => {
+    setRandomRestaurant(
+      restaurants[Math.floor(Math.random() * restaurants.length)]
+    );
+  };
+
+  console.log(randomRestaurant);
 
   useEffect(() => {
     fetchRestaurantsData();
@@ -21,8 +30,21 @@ function Restaurants() {
   return (
     <div className="restaurants">
       <h1>Restauranter i Trondheim</h1>
+      <div>
+        <button
+          className="random-button"
+          onClick={() => getRandomRestaurant(restaurants)}
+        >
+          random restaurant
+        </button>
+        {randomRestaurant && (
+          <>
+            <h1>{randomRestaurant.navn}</h1>
+          </>
+        )}
+      </div>
       <div className="restaurants-wrapper">
-        {restaurants.length > 0 && (
+        {restaurants && (
           <>
             {restaurants.map((restaurant, index) => (
               <Restaurant key={index} restaurant={restaurant} />
